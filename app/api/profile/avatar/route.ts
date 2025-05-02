690b7700c2228ec700f9b5893cf2aab76ef66213
sindyanjink <jalal@jalals-MacBook-Air.local>
2025-05-02 16:49:16 +0700

    fix:upload image blob

--- app/api/profile/avatar/route.ts
+++ app/api/profile/avatar/route.ts
@@ -4,19 +4,6 @@ import { verifyToken } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
 
 export async function PUT(req: NextRequest) {
-    const contentType = req.headers.get('content-type') || ''
-
-    // Cek apakah Content-Type sesuai
-    if (
-        !contentType.includes('multipart/form-data') &&
-        !contentType.includes('application/x-www-form-urlencoded')
-    ) {
-        return NextResponse.json(
-            { error: 'Content-Type must be multipart/form-data or application/x-www-form-urlencoded' },
-            { status: 400 }
-        )
-    }
-
     const token = req.headers.get("authorization")?.split(" ")[1];
     const user = verifyToken(token || "");
 
@@ -25,30 +12,27 @@ export async function PUT(req: NextRequest) {
     }
 
     try {
-        const formData = await req.formData()
-        const file = formData.get('file') as File | null
+        const formData = await req.formData();
+        const file = formData.get("file");
 
-        if (!file) {
-            return NextResponse.json({ error: 'No file uploaded' }, { status: 400 })
+        if (!file || typeof file === "string") {
+            return NextResponse.json({ error: "No valid file uploaded" }, { status: 400 });
         }
 
-        // Upload ke Vercel Blob
-        const blob = await put(file.name, file, {
-            access: 'public',
-        })
+        const blob = await put(file.name || "avatar.jpg", file.stream(), {
+            access: "public",
+        });
 
-        if(blob.url){
-            await prisma.user.update({
-                where : {id : user.id},
-                data : {
-                    avatar : blob.url
-                }
-            })
-        }
+        await prisma.user.update({
+            where: { id: user.id },
+            data: {
+                avatar: blob.url,
+            },
+        });
 
-        return NextResponse.json({ msg : "Success update avatar", url: blob.url })
+        return NextResponse.json({ msg: "Success update avatar", url: blob.url });
     } catch (error) {
-        console.error('Upload error:', error)
-        return NextResponse.json({ error: 'Failed to upload file' }, { status: 500 })
+        console.error("Upload error:", error);
+        return NextResponse.json({ error: "Failed to upload file" }, { status: 500 });
     }
 }
